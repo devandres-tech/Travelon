32d8cfcc35b6a55e6aaacc865ab9f59e17bd3642
andres alcocer <andresalcocer7@yahoo.com>
2019-03-28 10:06:44 -0400

    we can search for time

--- src/components/SideDetails.js
+++ src/components/SideDetails.js
@@ -1,17 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import moment from 'moment';
+import axios from 'axios';
+import tz from 'tz-lookup';
 
-function SideDetails(props) {
-  console.log('props', props)
-  return (
-    <div className="details">
-      Side Details
-    </div>
-  )
+import { fetchTime } from '../actions/index';
+
+class SideDetails extends Component {
+
+  componentDidUpdate() {
+    this.getTime();
+  }
+
+  getTime = (coords) => {
+    if (this.props.country[0]) {
+      const coords = this.props.country[0].latlng;
+      const timeZ = tz(coords[0], coords[1]);
+      const options = {
+        timeZone: timeZ,
+        year: 'numeric', month: 'numeric', day: 'numeric',
+        hour: 'numeric', minute: 'numeric', second: 'numeric',
+      },
+        formatter = new Intl.DateTimeFormat([], options)
+      const time = formatter.format(new Date());
+      return time.slice(11, 21);
+    }
+  }
+
+  render() {
+    const time = this.getTime();
+    console.log('time', time);
+
+    if (this.props.country[0]) {
+
+    }
+
+    return (
+      <p>current time: {time}</p>
+    )
+  }
 }
 
 function mapStateToProps({ country }) {
   return { country }
 }
 
-export default connect(mapStateToProps)(SideDetails);
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ fetchTime }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SideDetails);
+
+
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import moment from 'moment';
+
+// const SideDetails = (props) => {
+
+//   if (!props.country[0]) {
+//     return <p>Loading...</p>
+//   }
+
+//   const country = props.country[0];
+//   const date = moment.utc(country.timezones[0]);
+
+//   console.log('date', date);
+//   console.log('country', country)
+//   return (
+//     <div className="details">
+//       <p>current time: {country.timezones[0]}</p>
+//     </div>
+//   )
+// }
+
+// function mapStateToProps({ country }) {
+//   return { country }
+// }
+
+// export default connect(mapStateToProps)(SideDetails);
--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -1,10 +1,14 @@
 import { GET_COUNTRY } from '../actions';
+import { FETCH_TIME } from '../actions';
 
 export default function (state = [], action) {
   switch (action.type) {
     case GET_COUNTRY:
-      console.log('data', action.payload.data[0])
       return [action.payload.data[0], ...state];
+    case FETCH_TIME:
+      console.log('time', action.payload);
+      return [action.payload, ...state]
+    default: state = [];
   }
 
   return state;
